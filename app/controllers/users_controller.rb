094207252f2f53d54b34eab57220dc24df9df8af
Hot <sulejmanhot@gmail.com>
2020-02-03 16:07:51 +0100

    Improved styling and added logic to videos show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!, :except => [:index]
+
+def index
+
+end
 def show
   @videos = current_user.videos.order(created_at: :desc)
+  @user = User.find(params[:id])
 end
 end

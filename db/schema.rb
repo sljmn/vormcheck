aaa3076f6dc3aef66469a645a2949b612b1f882c
sljmn <sulejmanhot@gmail.com>
2020-02-04 21:58:05 +0100

    Added postgress

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_02_03_190109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 2020_02_03_190109) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "video_id"
-    t.integer "user_id"
+    t.bigint "video_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
